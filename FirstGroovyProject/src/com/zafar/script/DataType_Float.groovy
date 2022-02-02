47536738a169fa62da0cb01df05611c5bc2341f4
Zafar imam <zafarimam7032@gmail.com>
2022-02-02 11:47:10 +0530

    Integer Data Type

---
+++ FirstGroovyProject/src/com/zafar/script/DataType_Float.groovy
@@ -0,0 +1,8 @@
+package com.zafar.script
+
+float a=30.0;
+b=50.0;
+println a +" "+b
+
+println a.getClass()//class java.lang.Integer
+println b.getClass()//class java.lang.Integer
---
+++ FirstGroovyProject/src/com/zafar/script/DataType_Integer.groovy
@@ -0,0 +1,30 @@
+package com.zafar.script
+
+int a=30;
+b=50;
+short d=60
+byte e=70; //semi clon is optional
+long f=80;
+println f;
+println f.getClass()
+println e
+println e.getClass()
+println a +" "+b
+println d;
+println d.getClass()
+println a.getClass()//class java.lang.Integer
+println b.getClass()//class java.lang.Integer
+
+//max and min value of each data type
+println "=========================="
+
+println "Mx value of Integer:" +a.MAX_VALUE;
+println "min value of Integer:" +a.MIN_VALUE;
+println "max value of Integer:" +b.MAX_VALUE;
+println "min value of Integer:" +b.MIN_VALUE;
+println "max value of Short:" +d.MAX_VALUE;
+println "min value of Short:" +d.MIN_VALUE;
+println "max value of Byte:" +e.MAX_VALUE;
+println "min value of Byte:" +e.MIN_VALUE;
+println "max value of Long:" +f.MAX_VALUE;
+println "min value of Long:" +f.MIN_VALUE;
